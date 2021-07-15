e98a94faf12cf9a029906239293724bb8372d93e
Ben Kalsky <benkalsky@gmail.com>
2021-07-15 18:57:45 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,3 +21,18 @@ function hello_elementor_child_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+// Disable plugin auto-update email notification
+add_filter('auto_plugin_update_send_email', '__return_false');
+ 
+// Disable theme auto-update email notification
+add_filter('auto_theme_update_send_email', '__return_false');
+
+// Disable WordPress core auto-update email notification
+add_filter( 'auto_core_update_send_email', 'wpb_stop_auto_update_emails', 10, 4 );
+function wpb_stop_update_emails( $send, $type, $core_update, $result ) {
+	if ( ! empty( $type ) && $type == 'success' ) {
+		return false;
+	}
+	return true;
+}
